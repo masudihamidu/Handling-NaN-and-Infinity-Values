e86c40a46a6222fc76ced371eaaeab823467e6a5
masudihamidu <masudihmfaume@gmail.com>
2022-11-17 20:39:46 +0300

    button color style

--- Desktop/Weather/script/scriptfile.js
+++ Desktop/Weather/script/scriptfile.js
@@ -40,4 +40,12 @@ form.addEventListener("submit", e => {
         return;
         }
     }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const {main, name, sys, weather} = data;
+        const icon = `https://s3-us-west-2-amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+    })
 })
