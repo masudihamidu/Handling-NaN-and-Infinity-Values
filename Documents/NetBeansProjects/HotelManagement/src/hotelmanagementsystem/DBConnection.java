f312f70d690314697e888d3bc0420607dd682731
masudihamidu <masudihmfaume@gmail.com>
2023-05-15 20:45:35 +0300

    communication be

---
+++ Documents/NetBeansProjects/HotelManagement/src/hotelmanagementsystem/DBConnection.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelmanagementsystem;
+import java.sql.*;
+/**
+ *
+ * @author I.A.T COMPUTERS LLC
+ */
+
+
+public class DBConnection {
+    Connection conn;
+    PreparedStatement pst;
+    ResultSet result;
+    
+    
+    
+}
